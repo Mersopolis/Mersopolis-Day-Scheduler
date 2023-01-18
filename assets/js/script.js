3e6c0a113f4bafb290425081e6ffadd86fc83ffb
Mersopolis <mersopolis@gmail.com>
2023-01-17 23:05:46 -0700

    Saving Events. Comments.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,70 @@
-// Page functions run only after page is fully loaded
+// Functions run only after page is fully loaded
 $(function () {
   // Defines element variables
-  var timeBlock = $(".time-block");
-  var DateAndTimeEl = $('#DateAndTime');
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  var timeBlockEl = $(".time-block");
+  var DateAndTimeEl = $("#DateAndTime");
 
-  // Defines current hour variable
+  // Saves event text to localStorage when clicking corresponding save button
+  $(".saveBtn").on("click", function() {
+    var key = JSON.stringify("eventStorage" + $(this).parent().attr("id"))
+    var value = JSON.stringify($(this).siblings("textarea").val())
+      localStorage.setItem(key, value);
+    });
+
+  // Defines timeBlockColor function
+  function timeBlockColor() {
+  // Sets current hour from DayJS as a variable
   var hour = dayjs().hour();
-
-  // Toggles time block color based on whether it is past, present, or future
-  for (var i = 0; i < timeBlock.length; i++) {
-    if (timeBlock[i].id < hour) {
-      timeBlock[i].classList.remove("present", "future");
-      timeBlock[i].classList.add("past");
-    }
-    else if (timeBlock[i].id == hour) {
-      timeBlock[i].classList.remove("past", "future");
-      timeBlock[i].classList.add("present");
-    }
-    else if (timeBlock[i].id > hour) {
-      timeBlock[i].classList.remove("past", "present");
-      timeBlock[i].classList.add("future");
-    }
-    else {
-      console.log("timeBlock failure");
+  // Toggles time block color based on whether the hour is past, present, or future
+    for (var i = 0; i < timeBlockEl.length; i++) {
+      if (timeBlockEl[i].id < hour) {
+        timeBlockEl[i].classList.remove("present", "future");
+        timeBlockEl[i].classList.add("past");
+      }
+      else if (timeBlockEl[i].id == hour) {
+        timeBlockEl[i].classList.remove("past", "future");
+        timeBlockEl[i].classList.add("present");
+      }
+      else if (timeBlockEl[i].id > hour) {
+        timeBlockEl[i].classList.remove("past", "present");
+        timeBlockEl[i].classList.add("future");
+      }
+      else {
+        console.log("timeBlockColor failure");
+      }
     }
   }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // Runs timeBlockColor on page load and updates every second
+  setInterval(timeBlockColor, 1000);
 
-// Displays current date and time
+  // Defines displayTime function
   function displayTime() {
+    // Sets current date and time from DayJS as a variable in a certain format
     var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    // Displays current date and time
     DateAndTimeEl.text(rightNow);
   }
-  displayTime();
+  // Runs displayTime on page load and updates every second
   setInterval(displayTime, 1000);
+  
+  // Defines getEventsFromStorage function
+  function getEventsFromStorage() {
+    for (var i = 0; i < timeBlockEl.length; i++) {
+      var eventStorage = localStorage.getItem("eventStorage" + i);
+      // Writes events from localStorage, if any
+      if (eventStorage) {
+        for (var i = 0; i < timeBlockEl.length; i++) {
+          var eventTextEl = timeBlockEl[i].children("textarea");
+          eventTextEl = JSON.parse(eventStorage);
+        }
+      // Returns an empty array if there aren't any events.
+      } else {
+        eventTextEl = [];
+      }
+      return eventTextEl;
+    }
+  }
+  // Displays events from localStorage on page load
+  getEventsFromStorage();
+
 });
